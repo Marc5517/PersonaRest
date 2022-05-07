d97783e07115b2ea9079013dedc6c1437563537e
Marc Steen Fordsmand <marc5517@edu.easj.dk>
2022-05-07 21:09:51 +0200

    Add project files.

---
+++ PersonaRest/Controllers/PersonasController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonaRest.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PersonaRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        private static readonly List<Persona> Personas = new List<Persona>()
+        {
+            new Persona(1, "Arsene", 1, "Fool"),
+            new Persona(2, "Pixie", 2, "Lovers"),
+            new Persona(3, "Jack O' Lantern", 2, "Magician"),
+            new Persona(4, "Zorro", 3, "Magician"),
+            new Persona(5, "Bicorn", 4, "Hermit"),
+            new Persona(6, "Agathion", 3, "Chariot"),
+            new Persona(7, "Captain Kidd", 5, "Chariot")
+        };
+
+        // GET: api/<PersonasController>
+        [HttpGet]
+        public IEnumerable<Persona> Get()
+        {
+            return Personas;
+        }
+
+        // GET api/<PersonasController>/5
+        [HttpGet]
+        [Route("{personaId}")]
+        public Persona Get(int personaId)
+        {
+            return Personas.Find(p => p.PersonaId == personaId);
+        }
+
+        [HttpGet]
+        [Route("arcana/{arcana}")]
+        public IEnumerable<Persona> GetByArcana(string arcana)
+        {
+            List<Persona> lPersonas = Personas.FindAll(p => p.Arcana.Equals(arcana));
+            return lPersonas;
+        }
+
+        [HttpGet]
+        [Route("level/{level}")]
+        public IEnumerable<Persona> GetByLevel(int level)
+        {
+            List<Persona> lPersonas = Personas.FindAll(p => p.Level.Equals(level));
+            return lPersonas;
+        }
+
+        [HttpGet]
+        [Route("level/{level}/arcana/{arcana}")]
+        public IEnumerable<Persona> GetByLevelAndArcana(int level, string arcana)
+        {
+            List<Persona> lPersonas = Personas.FindAll(p => p.Level.Equals(level));
+            List<Persona> lXPersonas = lPersonas.FindAll(p => p.Arcana.Equals(arcana));
+            return lXPersonas;
+        }
+
+        // POST api/<PersonasController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PersonasController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonasController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PersonaRest/Model/Persona.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonaRest.Model
+{
+    public class Persona
+    {
+        private int _personaId;
+        private string _name;
+        private int _level;
+        private string _arcana;
+
+        public Persona()
+        {
+
+        }
+
+        public Persona(int personaId, string name, int level, string arcana)
+        {
+            _personaId = personaId;
+            _name = name;
+            _level = level;
+            _arcana = arcana;
+        }
+
+        public int PersonaId
+        {
+            get => _personaId;
+            set
+            {
+                _personaId = value;
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int Level
+        {
+            get => _level;
+            set
+            {
+                _level = value;
+            }
+        }
+
+        public string Arcana
+        {
+            get => _arcana;
+            set
+            {
+                _arcana = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(PersonaId)}: {_personaId}, {nameof(Name)}: {_name}, {nameof(Level)}: {_level}, {nameof(Arcana)}: {_arcana}";
+        }
+    }
+}
